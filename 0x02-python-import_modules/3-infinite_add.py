e1460020ff99ea56b96c8ebb0369a7d8ce5485c8
flamonrose23 <fatinemar20@icloud.com>
2024-01-04 15:31:21 -0800

    Task 3 update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,12 @@
 if __name__ == "__main__":
     import sys
 
+    argc = len(sys.argv) - 1
+    x = 0
     total = 0
-    for x in range(len(sys.argv) - 1):
-        total += int(sys.argv[x + 1])
-        print("{}".format(total))
+    for arg in sys.argv:
+        if c != 0:
+            total = total + int(arg)
+        else:
+            x = x + 1
+        print("{:d}".format(result))
