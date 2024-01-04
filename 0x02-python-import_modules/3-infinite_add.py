c171a1a00e17705b3a8c727d725a3eda9490d9db
flamonrose23 <fatinemar20@icloud.com>
2024-01-04 15:56:06 -0800

    Task 3 corrected

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    def add_arg(argv):
-        n = len(argv) - 1
-        if n == 0:
-            print("{:d}".format(n))
-            return
-        else:
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
         x = 1
         add = 0
         while x <= n:
             add += int(argv[x])
             x += 1
-        print("{:d}".format(add))
+            print("{:d}".format(add))
+
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
