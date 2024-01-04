6ed334b21df30742e6ae6dd4c5f74e6722ac8aa4
flamonrose23 <fatinemar20@icloud.com>
2024-01-04 15:48:28 -0800

    Task 4 updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 import sys
 import hidden_4 as hidden
 
-if __name__ != "__main__":
+if __name__ == "__main__":
     exit()
 
 for name in dir(hidden):
-    if name[0:2] != "__":
+    if name[0:2] == "__":
         print(name)
