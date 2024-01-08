b69d1ffad1cf457b4717a965e4f5d040471106b8
flamonrose23 <fatinemar20@icloud.com>
2024-01-08 11:50:18 -0800

    Tasks 10 11 12 corrected

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for number, x in enumerate(my_list):
-        if x % 2 == 0:
-            boolist[number] = True
-        else:
-            boolist[number] = False
-    return(boolist)
+    new_list = []
+    if my_list:
+        for num in my_list:
+            new_list.append(False if num % 2 else True)
+        return new_list
