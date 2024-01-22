8272642932530516bae4a75d85094028a9816ac8
flamonrose23 <fatinemar20@icloud.com>
2024-01-22 12:28:37 -0800

    Task 2 function printing first x elements oflist & integers corrected

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    number = 0
+    count = 0
     for m in range(0, x):
         try:
             print("{:d}".format(my_list[m]), end="")
-            number += 1
+            count += 1
         except (TypeError, ValueError):
             continue
         print("")
-        return number
+        return count
