f6c7d82c52718d2c846a5bc9f861c145db8068f5
flamonrose23 <fatinemar20@icloud.com>
2024-01-23 13:19:13 -0800

    Task 3 test

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """class square defining a square"""
+
+    def __init__(self, size=0):
+        """initializing another square
+        Args: representing size of this class square
+        Raises: TypeError & ValueError
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("exception if size is not an integer")
+        if size < 0:
+            raise ValueError("exception if size >= 0")
+
+        self.__size = size
+
+
+def area(self):
+    """
+    Return: current area square
+    """
+    return (self.__size * self.__size)
