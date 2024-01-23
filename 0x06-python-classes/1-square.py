03f8902e523ee470de4a5dd4cebc9147d84a7a7d
flamonrose23 <fatinemar20@icloud.com>
2024-01-23 12:45:54 -0800

    Task 1 square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+
+class Square:
+    """class square defining a square"""
+
+    def __init__(self, size):
+        """initializing another square
+        Args: representing size of this class square
+        """
+        self.__size = size
