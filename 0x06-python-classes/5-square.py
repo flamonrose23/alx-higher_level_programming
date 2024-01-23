7aff1881a6c103a4b74ed2b574198dc31bf6e13b
flamonrose23 <fatinemar20@icloud.com>
2024-01-23 15:43:08 -0800

    Task 5 print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""A module that defines a square """
+
+
+class Square:
+    """A class representing a square"""
+
+    def __init__(self, size=0):
+        """Initializing new square class
+        Args:
+            size: representing size of new square defined
+        Raises:
+            TypeError & ValueError
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieve current size of the square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size should be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Returns area  square of size
+        """
+
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Printing in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+
+        for x in range(self.__size):
+            print("#" * self.__size)
