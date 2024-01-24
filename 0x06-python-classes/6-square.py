6bcc8b2e06d5c62805f2db26aa26c893ada72e55
flamonrose23 <fatinemar20@icloud.com>
2024-01-23 16:25:16 -0800

    Task 6 corrected

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,3 +69,9 @@ class Square:
         """
         if self.__size == 0:
             print()
+
+        [print("") for x in range(0, self.__position[1])]
+        for x in range(0, self.__size):
+            [print(" ", end="") for y in range(0, self.__position[0])]
+            [print("#", end="") for z in range(0, self.__size)]
+            print()
