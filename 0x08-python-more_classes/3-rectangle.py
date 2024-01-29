5f6d417d0ed8801a3efadc7b239ef98bf10f70a3
flamonrose23 <fatinemar20@icloud.com>
2024-01-29 10:32:00 -0800

    Task 3 correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Writing a class Rectangle definied on the first task
+Writing a class Rectangle
 """
 
 
@@ -21,8 +21,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """definining width of the rectangle"""
-        if not isinstance(value, int):
+        """
+        definining width of the rectangle
+        """
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -37,8 +39,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """defining height of the rectangle"""
-        if not isinstance(value, int):
+        """
+        defining height of the rectangle
+        """
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -56,8 +60,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return 0
-        else:
-            return (self.__width * 2) + (self.__height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """
