685c38b99c0c8d8c3dddb3617731642b1e063830
flamonrose23 <fatinemar20@icloud.com>
2024-02-05 15:18:27 -0800

    Task 10 Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Writing class Square
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Representing class Square inherited
+    """
+    def __init__(self, size):
+        """
+        Initializing a Square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returning area of Square
+        """
+        return self.__size * self.__size
