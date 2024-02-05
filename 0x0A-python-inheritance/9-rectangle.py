68c9c11dd040544ba9f9045bc409ddd5454c281b
flamonrose23 <fatinemar20@icloud.com>
2024-02-05 15:03:41 -0800

    Task 9 Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Writing class Rectangle
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Representing Rectangle by base of geometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializing a Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Returning area of Rectangle
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Returning Rectangle description and the print
+        """
+        return "[Rectangle] {}/{}".format(self.__height, self.__width)
