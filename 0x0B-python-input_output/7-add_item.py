b3f85047129eb7676de79a5d9bf788420d778f59
flamonrose23 <fatinemar20@icloud.com>
2024-02-06 09:50:48 -0800

    Task 7 corrected

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,8 @@ Writing script adding all arguments
 """
 
 
-from os import path
+import json
+import os.path
 import sys
 
 
@@ -12,8 +13,15 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-my_list = []
-if path.isfile("add_item.json"):
-    my_list = load_from_json_file("add_item.json")
+nw_list = []
 
-save_to_json_file(my_list + sys.argv[1:], "add_item.json")
+
+name_file = "add_item.json"
+
+
+if os.path.exists(name_file) and os.path.getsize(name_file) > 0:
+    nw_list = load_from_json_file(name_file)
+
+if len(sys.argv) > 1:
+    for element in sys.argv[1:]:
+        nw_list.append(element)
