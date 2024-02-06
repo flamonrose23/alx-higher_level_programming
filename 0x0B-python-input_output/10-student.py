e4ac0110b33a0c4e9b9852333ae3c3430b89872c
flamonrose23 <fatinemar20@icloud.com>
2024-02-06 09:31:41 -0800

    Task 10 Student to JSON with filter corrected

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
-"""Module 10-student.
-Creates a Student class.
+"""
+Writing class Student defined by
+Public instance attributes:
+    first name, last name && age
 """
 
 
 class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
+    """
+    Representing a student
     """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
+        """
+        Initializing a Student by instance
+        """
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation
-        of a Student instance.
-
-        Args:
-            - attrs: list of attributes
-
-        Returns: the dict representation of the instance.
+        """
+        Retrieving dict representation
+        of a Student instance
         """
 
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
-            return my_dict
+        new_dict = dict()
+        if type(attrs) is list and all(type(x) is str for j in attrs):
+            for j in attrs:
+                if j in self.__dict__:
+                    new_dict.update({j: self.__dict__[j]})
+            return new_dict
         return self.__dict__.copy()
