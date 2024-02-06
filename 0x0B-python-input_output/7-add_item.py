32993d1b57781838a9ef285665d6e5c983fa1481
flamonrose23 <fatinemar20@icloud.com>
2024-02-06 08:27:17 -0800

    Task 7 done

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Writing script adding all arguments
+"""
+
+
+import json
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+my_list = []
+if path.isfile("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+
+save_to_json_file(my_list + sys.argv[1:], "add_item.json")
