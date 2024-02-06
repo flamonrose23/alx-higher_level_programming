4919eba3ed8e551b33a496c3d63c7710dcaef071
flamonrose23 <fatinemar20@icloud.com>
2024-02-06 10:13:11 -0800

    Task 12 Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Creating function of Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returning list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+
+    for x in range(1, n):
+        row = [1]
+        for y in range(1, x):
+            row.append(triangles[x - 1][y - 1] + triangles[x - 1][y])
+        row.append(1)
+        triangles.append(1)
+
+    return (triangles)
