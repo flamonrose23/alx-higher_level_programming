959c1f43d479d60fb10fbe858563067670e4d9d9
flamonrose23 <fatinemar20@icloud.com>
2024-02-06 10:43:17 -0800

    Task 13 Adv: Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Writing function for seatch & update
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserting line of text to a file
+    containing a specific string
+    """
+    nw_text = ""
+
+    with open(filename) as f:
+        for line in f:
+            nw_text += line
+            if search_string in line:
+                nw_text += new_string
+
+    with open(filename, "+w") as f:
+        w.write(nw_text)
