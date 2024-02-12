8af2779ad1dcb73085927f1901c2a833e3ccf1b2
flamonrose23 <fatinemar20@icloud.com>
2024-02-12 14:38:51 -0800

    Updating some tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,5 +36,41 @@ class Square(Rectangle):
         """
         Returning the print and string of Rectangle
         """
-        return "[Square] ({}) {}/{} - {}".format(
-                self.id, self.x,  self.width, self.y)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
+    def update(self, *args, **kwargs):
+        """
+        Updating class Square by adding public method
+        *args: arguments of no-keyworded attributes
+        **kwargs: arguments of key or valour attributes
+        """
+        if args and len(args) != 0:
+            u = 0
+            for ar in args:
+                if u == 0:
+                    if ar is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = ar
+                elif u == 1:
+                    self.size = ar
+                elif u == 2:
+                    self.x = ar
+                elif u == 3:
+                    self.y = ar
+                u += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for i, e in kwargs.items():
+                if i == "id":
+                    if e is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = e
+                elif i == "size":
+                    self.size = e
+                elif i == "x":
+                    self.x = e
+                elif i == "y":
+                    self.y = e
