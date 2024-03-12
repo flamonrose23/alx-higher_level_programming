58f1981d6e6e9e682fd18fb23b463b9827d5adcb
flamonrose23 <fatinemar20@icloud.com>
2024-03-12 15:15:16 -0700

    Task 16 corrected

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,4 @@
 -- Writing script listing all records of the second_table
 -- Records should be listed by descending score
 SELECT score, name FROM secomd_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+WHERE name IS NOT NULL ORDER BY score DESC;
