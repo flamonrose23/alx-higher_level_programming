f1b96c81842f5f0b992570cb13db4fa78829ac80
flamonrose23 <fatinemar20@icloud.com>
2024-03-12 14:08:53 -0700

    Task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Writing sript creating table called first_table
+-- This table created in the current database of the server
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+	);
