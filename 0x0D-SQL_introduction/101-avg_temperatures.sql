38e79312cd76a447757ea7127dc051360613dff3
flamonrose23 <fatinemar20@icloud.com>
2024-03-12 15:33:13 -0700

    Task Adv 18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Writing script displaying average temperature (Fahrenheit)
+-- by city ordered by temperature (descending)
+SELECT city, AVG(value) AS abg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
