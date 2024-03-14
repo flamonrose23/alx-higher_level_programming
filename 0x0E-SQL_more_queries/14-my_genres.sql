d47faed6d6ba68fc0027bd36ab5c6bf52626c23c
flamonrose23 <fatinemar20@icloud.com>
2024-03-13 18:53:02 -0700

    Task 14: My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Writing script using hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- tv_shows table contains only one record where title = Dexter
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+
+SELECT tv_genres.name FROM tv_show_genres INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
