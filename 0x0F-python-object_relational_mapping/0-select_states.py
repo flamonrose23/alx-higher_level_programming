11c4fcc707eee9e26e044e77a61c9159c839d8a6
flamonrose23 <fatinemar20@icloud.com>
2024-04-14 08:41:14 -0700

    Testing task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 
+Writing Script listing all states from database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    curent = db.cursor()
+    curent.execute("SELECT * FROM states")
+
+    rows = curent.fetchall()
+    for x in rows:
+        print(x)
+    curent.close()
+    db.close()
