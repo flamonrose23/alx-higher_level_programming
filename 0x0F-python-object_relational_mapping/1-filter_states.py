2670cc7dde5628cf7a3629111aac641df0c03bff
flamonrose23 <fatinemar20@icloud.com>
2024-04-14 11:52:50 -0700

    fixing error task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
+
 import MySQLdb
 from sys import argv
 
-# The code should not executed when it is imported
+# The code should not be executed when imported
 if __name__ == '__main__':
-    # connection to database
+    # maing connection to database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # ability to have multiple seperate working environments
-    # through same connection t database.
-    curent = db.cursor()
+    # giving the ability to have multiple seperate working environments
+    # through same connection to the database.
+    curt = db.cursor()
 
-    curent.execute("SELECT * FROM states WHERE name\
+    curt.execute("SELECT * FROM states WHERE name\
                 LIKE BINARY 'N%' ORDER BY id ASC")
 
-    rows = curent.fetchall()
+    rows = curt.fetchall()
     for x in rows:
         print(x)
-    # Clean all process
-    curent.close()
+    # Clean up process
+    curt.close()
     db.close()
